b7a0aaa765b5be14152e84e7e5a89cc5623fbec5
isadorabello <isadora.bello@gec.inatel.br>
2022-11-05 19:18:30 -0300

    feat: bank class remade

--- DesignPattern/src/main/java/org/example/Santander.java
+++ DesignPattern/src/main/java/org/example/Santander.java
@@ -1,4 +1,9 @@
 package org.example;
 
+import org.example.juros.JurosSantander;
+
 public class Santander extends CalculadoraDeJuros{
+    public Santander() {
+        calculaJuros = new JurosSantander();
+    }
 }
